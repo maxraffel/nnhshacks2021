c9990c98c2cef24e11d9b3d2ae90a6fed5a31532
GodzillaHunter42 <44725991+GodzillaHunter42@users.noreply.github.com>
2021-11-13 16:42:50 -0500

    webscrapey stuff idkman no it does not work

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('drinky');
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({defaultViewport: null});
+    const page = await browser.newPage();
+    await page.goto('https://www.fdmealplanner.com/', {waitUntil: 'networkidle2'});
+    await page.type('.searchInput', 'newton', { delay: 10 });
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.waitForTimeout(2000);
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.waitForTimeout(2000);
+    //await page.click('.menuDetails');
+    //console.log(await page.$$eval('.menuDetails', (item) => item.length));
+    const items = Array.from(await page.$$('.menuDetails'));
+    //console.log(Array.from(items));
+
+
+    for (const i of items) {
+        console.log(i.length);
+        /*for (const x of Array.from(i.children)) {
+            console.log(x.innerHTML);
+        }*/
+    }
+    await page.screenshot({ path: 'abdungis.png', waitUntil: 'networkidle2'});
+    await browser.close();
+})();
