5c197453b8a81d71778ae2b4d948439fe460e407
Eric Shen <94233458+EricShen9@users.noreply.github.com>
2021-11-14 13:48:51 -0500

    po
    bungis

---
+++ index2.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer');
+const Menujs = require('./menu.js');
+// import { Menu as Menu, Item as Item} from '/menu.js'
+
+(async () => {
+    const browser = await puppeteer.launch({defaultViewport: null});
+    const page = await browser.newPage();
+    await page.goto('https://www.fdmealplanner.com/', {waitUntil: 'networkidle2'});
+    await page.type('.searchInput', 'newton', { delay: 10 });
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.waitForTimeout(2000);
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.keyboard.press('Tab', {delay: 50});
+    await page.keyboard.press('Enter', {delay: 50});
+    await page.waitForTimeout(3000);
+    //await page.click('.menuDetails');
+    //console.log(await page.$$eval('.menuDetails', (item) => item.length));
+    
+    const items = await page.$$('.tempMenuItemMonthly');
+    
+
+
+    let summativeMenuArr = [];
+    for (const item of items) {
+
+        //ayo max this is sleepy max speaking u gotta do .click on each item, check for everything labeled nutridetial or value or whatever idkman and then hit escape for ez dubs
+        //just like do it here cuz that makes sense and then put all the stuff into item objects and pop those into a meal object for each meal and then write some methods
+        //then just connect it up to website and boom yay you did it high five its 2am please help me :)))
+
+        //gets the innerhtml of everything then makes it into a string then does basic formatting
+        let text = await page.evaluate(el => el.innerText, item);
+        let texter = JSON.stringify(text);
+        //checks to see if it is empty and skips it if so
+        if (texter.length <= 2) { continue }
+        //removes " from either side of the string since those are always there
+        texter = texter.substring(1);
+        texter = texter.substring(0, texter.length - 1);
+        let menuArr = texter.split('\\n');
+        for (let i = 0; i < menuArr.length; i++) {
+            menuArr[i] = menuArr[i].trim();
+            if (menuArr[i] === '') {
+                menuArr.splice(i, 1);
+                i -= 1;
+            }
+        }
+        summativeMenuArr.push(Menujs.Menu.createMenuForToday(menuArr));
+    }
+    console.log(summativeMenuArr);
+    
+    await page.screenshot({ path: 'abdungis.png', waitUntil: 'networkidle2'});
+    await browser.close();
+})();
--- menu.js
+++ menu.js
@@ -1,29 +1,78 @@
-export default class Menu {
-    constructor(entrees, sides, condiments, beverages, condiments = []) {
+class Menu {
+    constructor(entrees, sides, beverages, condiments = []) {
         this.entrees = entrees;
         this.sides = sides;
         this.beverages = beverages;
         this.condiments = condiments;
     }
+
+    static createMenuForToday(menuArr){
+        let entrees = [];
+        let sides = [];
+        let beverages = [];
+        let condiments = [];
+        let currentFoodItem = 'entree';
+        for(const menuItem of menuArr){
+            switch(menuItem.toLowerCase()){
+                case 'entree' :
+                    currentFoodItem = 'entree';
+                    break;
+                case 'side' :
+                    currentFoodItem = 'side';
+                    break;
+                case 'condiment' :
+                    currentFoodItem = 'condiment';
+                    break;
+                case 'beverage' :
+                    currentFoodItem = 'beverage';
+                    break;
+                default :
+                    if(currentFoodItem === 'entree'){
+                        // entrees.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
+                        entrees.push(menuItem);
+                    } else if(currentFoodItem === 'side'){
+                        // sides.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
+                        sides.push(menuItem);
+                    } else if(currentFoodItem === 'condiment'){
+                        // condiments.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
+                        condiments.push(menuItem);
+                    } else if(currentFoodItem === 'beverage'){
+                        // beverages.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
+                        beverages.push(menuItem);
+                    }
+                    break;
+            }
+        }
+        return new Menu(entrees, sides, beverages, condiments);
+    }
 }
 
-export class Item {
+class Item {
     //holy crap this was a pain to write
     constructor(name, calories, saturatedFat, cholesterol, carbohydrates, totalSugars, vitaminA, calcium, fat, transFattyAcid, sodium, dietaryFiber, protein, vitaminC, iron) {
         this.name = name;
-        this.calories = calories;
-        this.saturatedFat = saturatedFat;
-        this.cholesterol = cholesterol;
-        this.carbohydrates = carbohydrates;
-        this.totalSugars = totalSugars;
-        this.vitaminA = vitaminA;
-        this.calcium = calcium;
-        this.fat = fat;
-        this.transFattyAcid = transFattyAcid;
-        this.sodium = sodium;
-        this.dietaryFiber = dietaryFiber;
-        this.protein = protein;
-        this.vitaminC = vitaminC;
-        this.iron = iron;
+        this.calories = calories; //1
+        this.saturatedFat = saturatedFat; //2
+        this.cholesterol = cholesterol; //3
+        this.carbohydrates = carbohydrates; //4
+        this.totalSugars = totalSugars; //5
+        this.vitaminA = vitaminA; //6
+        this.calcium = calcium; //7
+        this.fat = fat; //8
+        this.transFattyAcid = transFattyAcid; //9
+        this.sodium = sodium; //10
+        this.dietaryFiber = dietaryFiber; //11
+        this.protein = protein; //12
+        this.vitaminC = vitaminC; //13
+        this.iron = iron; //14
     }
 }
+
+module.exports = {
+    Menu : Menu,
+    Item : Item
+}
+
+
+
+
