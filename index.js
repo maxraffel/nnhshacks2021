5ba5a74a517548ed35285edfa7b8380c1175a2e2
GodzillaHunter42 <44725991+GodzillaHunter42@users.noreply.github.com>
2021-11-14 14:35:05 -0500

    it has the correct nutritional information, just matched to wrong name sometimes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
-import { Menu, Item } from '/menu.js'
+const { Menu } = require('./menu');
+const menu = require('./menu');
+//import { Menu, Item } from '/menu.js'
 
 (async () => {
     const browser = await puppeteer.launch({defaultViewport: null});
@@ -18,15 +20,79 @@ import { Menu, Item } from '/menu.js'
     await page.waitForTimeout(3000);
     //await page.click('.menuDetails');
     //console.log(await page.$$eval('.menuDetails', (item) => item.length));
+    /*
+    const links = await page.$$('a.menuDetails');
+    //console.log(links);
+    let i = 0;
+    links.forEach(async val => {
+        try {
+            await val.click();
+            page.screenshot({path: `dwaboutit/click1-${i}.png`, fullPage: true});
+            await val.click();
+            i += 1;
+            await page.waitForTimeout(500);
+            console.log('defining nutriDetails');
+            const nutriDetails = await page.$$('li.nutrientsDetails');
+            for (const nutriDetail of nutriDetails) {
+                console.log('evaluating nutriDetail');
+                const val = await page.evaluate(el => el.innerText, nutriDetail);
+                const values = JSON.stringify(val);
+                console.log(values);
+                await page.waitForTimeout(500);
+                await page.keyboard.press('Escape');
+            }
+            } catch (error) {
+                //console.log('oops');
+            }
+            
+    });*/
     
     const items = await page.$$('.tempMenuItemMonthly');
     
-
-
     //console.log(Array.from(items));
     //let menuArr = [];
+    let totalMenuArr = [];
+    let bigNutriArr = [];
+    //for each day
     for (const item of items) {
+        const menuItems = await page.$$('.lineItem');
+        //for each item
+        for (const menuItem of menuItems) {
+        try {
+        await menuItem.click();
+        //await page.waitForTimeout(500);
+        } catch {
+            console.log('thing');
+        }
+        let nutriArr = [];
+        const nutriDetails = await page.$$('li.nutrientsDetails');
+        //for each nutritional detail
+        for (const nutriDetail of nutriDetails) {
+            const val = await page.evaluate(el => el.innerText, nutriDetail);
+            let values = JSON.stringify(val);
+            //removes " from either side of the string since those are always there
+            values = values.substring(1);
+            values = values.substring(0, values.length - 1);
+            //removes whitespace
+            values = values.replace(/\s+/g, '');
+            values = values.replace(':\\n', ' ');
+            //bro idk
+            //LGtM
+            values = values.replace('\\n', '');
+            values = values.replace('\\n', '');
+            values = values.replace('\\n', '');
+            values = values.replace('\\n', '');
 
+            values = values[0].toLowerCase() + values.slice(1);
+            values = values.split(' ');
+            nutriArr.push(values);
+            //await page.keyboard.press('Escape');
+        }
+        bigNutriArr.push(nutriArr);
+        //await page.screenshot({path: 'dood.png'});
+        await page.keyboard.press('Enter');
+    }
+        //await page.waitForTimeout(500);
         //ayo max this is sleepy max speaking u gotta do .click on each item, check for everything labeled nutridetial or value or whatever idkman and then hit escape for ez dubs
         //just like do it here cuz that makes sense and then put all the stuff into item objects and pop those into a meal object for each meal and then write some methods
         //then just connect it up to website and boom yay you did it high five its 2am please help me :)))
@@ -47,10 +113,18 @@ import { Menu, Item } from '/menu.js'
                 i -= 1;
             }
         }
-        console.log(menuArr);
+        const todayMenu = Menu.createMenuForToday(menuArr, bigNutriArr);
+        totalMenuArr.push(todayMenu);
+        console.log(todayMenu);
     }
+
+
+    
+    
+    
     //console.log(menuArr);
     
     await page.screenshot({ path: 'abdungis.png', waitUntil: 'networkidle2'});
     await browser.close();
 })();
+
--- menu.js
+++ menu.js
@@ -6,13 +6,14 @@ class Menu {
         this.condiments = condiments;
     }
 
-    static createMenuForToday(menuArr){
+    static createMenuForToday(menuArr, nutriArr){
         let entrees = [];
         let sides = [];
         let beverages = [];
         let condiments = [];
         let currentFoodItem = 'entree';
-        for(const menuItem of menuArr){
+        for(let i = 0; i < menuArr.length; i++){
+            const menuItem = menuArr[i]
             switch(menuItem.toLowerCase()){
                 case 'entree' :
                     currentFoodItem = 'entree';
@@ -29,16 +30,16 @@ class Menu {
                 default :
                     if(currentFoodItem === 'entree'){
                         // entrees.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
-                        entrees.push(menuItem);
+                        entrees.push(new Item(menuItem, nutriArr[i][0][1], nutriArr[i][1][1], nutriArr[i][2][1], nutriArr[i][3][1], nutriArr[i][4][1], nutriArr[i][5][1], nutriArr[i][6][1], nutriArr[i][7][1], nutriArr[i][8][1], nutriArr[i][9][1], nutriArr[i][10][1], nutriArr[i][11][1], nutriArr[i][12][1], nutriArr[i][13][1]));
                     } else if(currentFoodItem === 'side'){
                         // sides.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
-                        sides.push(menuItem);
+                        sides.push(new Item(menuItem, nutriArr[i][0][1], nutriArr[i][1][1], nutriArr[i][2][1], nutriArr[i][3][1], nutriArr[i][4][1], nutriArr[i][5][1], nutriArr[i][6][1], nutriArr[i][7][1], nutriArr[i][8][1], nutriArr[i][9][1], nutriArr[i][10][1], nutriArr[i][11][1], nutriArr[i][12][1], nutriArr[i][13][1]));
                     } else if(currentFoodItem === 'condiment'){
                         // condiments.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
-                        condiments.push(menuItem);
+                        condiments.push(new Item(menuItem, nutriArr[i][0][1], nutriArr[i][1][1], nutriArr[i][2][1], nutriArr[i][3][1], nutriArr[i][4][1], nutriArr[i][5][1], nutriArr[i][6][1], nutriArr[i][7][1], nutriArr[i][8][1], nutriArr[i][9][1], nutriArr[i][10][1], nutriArr[i][11][1], nutriArr[i][12][1], nutriArr[i][13][1]));
                     } else if(currentFoodItem === 'beverage'){
                         // beverages.push(new Menujs.Item(menuItem, '', '', '', '', '', '', '', '', '', '', '', '', '', ''));
-                        beverages.push(menuItem);
+                        beverages.push(new Item(menuItem, nutriArr[i][0][1], nutriArr[i][1][1], nutriArr[i][2][1], nutriArr[i][3][1], nutriArr[i][4][1], nutriArr[i][5][1], nutriArr[i][6][1], nutriArr[i][7][1], nutriArr[i][8][1], nutriArr[i][9][1], nutriArr[i][10][1], nutriArr[i][11][1], nutriArr[i][12][1], nutriArr[i][13][1]));
                     }
                     break;
             }
@@ -49,7 +50,7 @@ class Menu {
 
 class Item {
     //holy crap this was a pain to write
-    constructor(name, calories, saturatedFat, cholesterol, carbohydrates, totalSugars, vitaminA, calcium, fat, transFattyAcid, sodium, dietaryFiber, protein, vitaminC, iron) {
+    constructor(name, calories, fat, saturatedFat, transFattyAcid, cholesterol, sodium, carbohydrates, dietaryFiber, totalSugars, protein, vitaminA, vitaminC, calcium, iron) {
         this.name = name;
         this.calories = calories; //1
         this.saturatedFat = saturatedFat; //2
